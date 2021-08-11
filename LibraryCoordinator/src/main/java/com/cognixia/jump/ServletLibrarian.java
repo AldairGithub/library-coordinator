9360c14100e3c0e29c28550e471b5239be86e621
souzale2 <plemos16@email.com>
2021-08-10 19:34:45 -0500

    servlet first commit

---
+++ LibraryCoordinator/src/main/java/com/cognixia/jump/ServletLibrarian.java
@@ -0,0 +1,44 @@
+package com.cognixia.jump;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletLibrarian
+ */
+public class ServletLibrarian extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	@Override
+	public void init() throws ServletException{
+		
+		
+		
+		System.out.println("Starting things off");
+			
+	}
+	
+	//called when servlet unloaded and perform cleanup
+	@Override
+	public void destroy() {
+		
+		System.out.println("Finished using servlet, now being destroyed");
+		
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
