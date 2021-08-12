7da5d330f640f41d6b65dddec8ad9bb7669829ee
Protocycle <protocycle.rules@gmail.com>
2021-08-12 10:55:11 -0500

    Setup action methods

--- LibraryCoordinator/src/main/java/com/cognixia/jump/frontend/LibrarianLoginServlet.java
+++ LibraryCoordinator/src/main/java/com/cognixia/jump/frontend/LibrarianLoginServlet.java
@@ -1,6 +1,7 @@
 package com.cognixia.jump.frontend;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,52 +10,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.cognixia.jump.connector.ConnectionManager;
+
 public class LibrarianLoginServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	// private Library lib;
+	Connection conn;
 
 	public void init() {
 		// initialize LibraryDAO
 		// lib = new LibraryDAO();
+		// conn = ConnectionManager.getConnection();
+	}
+	
+	public void destroy() {
+		// destroy connection
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String action = request.getServletPath();
-		RequestDispatcher rd;
 		
 		switch (action) {
 
 		case "/admin/login":
-			String username = request.getParameter("username").trim();
-			String password = request.getParameter("password").trim();
-
-			if (username.isEmpty() || password.isEmpty()) {
-				String message = "Fields cannot be empty.";
-				request.setAttribute("message", message);
-				request.setAttribute("error", true);
-				rd = request.getRequestDispatcher("/librarian_pages/librarian-login.jsp");
-				rd.forward(request, response);
-				break;
-			}
-			
-			int id = login(username, password);
-
-			if (id < 1) {
-				String message = "Wrong username or password";
-				request.setAttribute("message", message);
-				request.setAttribute("error", true);
-				rd = request.getRequestDispatcher("/librarian_pages/librarian-login.jsp");
-				rd.forward(request, response);
-				break;
-			}
-			
-			HttpSession session = request.getSession();
-			session.setAttribute("username", username);
-			session.setAttribute("id", id);
-			response.sendRedirect("home");
+			login(request, response);
 			break;
 		
 		default:
@@ -69,7 +51,43 @@ public class LibrarianLoginServlet extends HttpServlet {
 	}
 	
 	
-	public int login(String username, String password) {
+	
+	
+	
+	
+	public void login(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		RequestDispatcher rd;
+		String username = req.getParameter("username").trim();
+		String password = req.getParameter("password").trim();
+
+		if (username.isEmpty() || password.isEmpty()) {
+			String message = "Fields cannot be empty.";
+			req.setAttribute("message", message);
+			req.setAttribute("error", true);
+			rd = req.getRequestDispatcher("/librarian_pages/librarian-login.jsp");
+			rd.forward(req, res);
+			return;
+		}
+		
+		int id = login(username, password);
+
+		if (id < 1) {
+			String message = "Wrong username or password";
+			req.setAttribute("message", message);
+			req.setAttribute("error", true);
+			rd = req.getRequestDispatcher("/librarian_pages/librarian-login.jsp");
+			rd.forward(req, res);
+			return;
+		}
+		
+		HttpSession session = req.getSession();
+		session.setAttribute("username", username);
+		session.setAttribute("id", id);
+		res.sendRedirect("home");
+	}
+	
+	public int login(String username, String password) throws ServletException, IOException {
+		
 		int id = 1;
 		//id = lib.getLibrarianId(username, password)
 		
